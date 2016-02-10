4a424a0e93be452d113df38f71bc8b9d34e3e47b
vonwao <vonwao@gmail.com>
2016-02-10 16:56:05 -0500

    Initial commit for mantra-dialogue.
    Make postlist table

--- client/configs/routes.jsx
+++ client/configs/routes.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {FlowRouter} from 'meteor/kadira:flow-router';
 import {mount} from 'react-mounter';
 
+import {Layout} from '/client/configs/theme.jsx';
+//import Links from '../../_home/components/links.jsx';
+
 import MainLayout from '/client/modules/core/components/layout.main.jsx';
 import PostList from '/client/modules/core/containers/postlist';
 import Post from '/client/modules/core/containers/post';
 import NewPost from '/client/modules/core/containers/newpost';
 
 export default function (injectDeps) {
-  const MainLayoutCtx = injectDeps(MainLayout);
+  const MainLayoutCtx = injectDeps(Layout);
 
   // Move these as a module and call this from a main file
   FlowRouter.route('/posts', {
--- client/modules/core/components/postlist.jsx
+++ client/modules/core/components/postlist.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
 const PostList = ({posts}) => (
+  <div className="bs-docs-section clearfix">
   <div className='postlist'>
-    <ul>
+    <table className="table table-striped table-hover ">
+      <thead>
+      <tr>
+        <th>Title</th>
+        <th>Column heading</th>
+        <th>Column heading</th>
+      </tr>
+      </thead>
+      <tbody>
       {posts.map(post => (
-        <li key={post._id}>
-          <a href={`/post/${post._id}`}>{post.title}</a>
-        </li>
-      ))}
+      <tr key={post._id}>
+        <td><a href={`/post/${post._id}`}>{post.title}</a></td>
+      </tr>
+        ))}
+      </tbody>
+    </table>
+    <ul>
     </ul>
+  </div>
   </div>
 );
 
