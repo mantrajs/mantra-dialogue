fe785872b30c7407ba24e877f58f1ae2ee6b11b6
bright-sea <minghai.zhao@gmail.com>
2016-02-11 14:08:17 -0500

    upgrade to meteor 1.3 beta 8

--- server/main.js
+++ server/main.js
@@ -1,4 +1,10 @@
+import publications from './publications';
+import methods from './methods';
 import addInitialData from './configs/initial_adds.js';
 import addInitialUsers from './configs/initial_users.js';
+
+publications();
+methods();
 addInitialData();
 addInitialUsers();
+
---
+++ server/publications/index.js
@@ -0,0 +1,9 @@
+import posts from './posts';
+import users from './users';
+import colors from './colors';
+
+export default function () {
+  posts();
+  users();
+  colors();
+}
